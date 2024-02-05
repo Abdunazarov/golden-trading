707df44db20d6392c2a841b4ab765321a28ed2cc
Abdunazarov <abdunazarovdior@gmail.com>
2024-02-06 03:30:54 +0800

    Created: migrations, schemas, POST /register endpoint

--- services/user_service.py
+++ services/user_service.py
@@ -17,24 +17,25 @@ async def get_user_by_username(session: AsyncSession, username: str):
     result = await session.execute(query)
     return result.scalar_one_or_none()
 
+
 async def create_user(session: AsyncSession, user: UserCreateBody):
     user_config = {
         "fio": user.fio,
         "birthday": user.birthday.isoformat() if user.birthday else None,
-        "tags": user.tags
+        "tags": user.tags,
     }
 
     new_user = {
         "username": user.username.lower(),
         "email": user.email,
         "hashed_password": bcrypt.hash(user.password),
-        "config": json.dumps(user_config)
+        "config": json.dumps(user_config),
     }
 
     stmt = (
         pg_insert(UserModel)
         .values(**new_user)
-        .on_conflict_do_nothing(index_elements=['username'])
+        .on_conflict_do_nothing(index_elements=["username"])
         .returning(UserModel.id)
     )
     result = await session.execute(stmt)
